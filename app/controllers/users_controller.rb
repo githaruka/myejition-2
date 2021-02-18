d42f04920926ee24b336feb3b914ce5230fbb16e
githaruka <hhhhharuka1020@gmail.com>
2021-02-18 10:27:22 +0900

    css、userコントローラーの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   def show
     @dictionary = Dictionary.find(params[:id])
-    @user = @dictionary.user
-    #@user = User.find(params[:id])
+    #@user = @dictionary.user
+    @user = User.find(params[:id])
     @dictionaries = @user.dictionaries
   end
 end
